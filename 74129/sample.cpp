f76c39897a4a5c489d284adfa4fcdbcddb6c2a3c
Karen Matoke <104322851+KarenMatoke@users.noreply.github.com>
2024-02-28 20:38:10 +0000

    Add files via upload

--- 74129/sample.cpp
+++ 74129/sample.cpp
@@ -1,1 +1,17 @@
-x
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define NILPTR NULL
+int main() {
+char *percentPtr = NILPTR;
+char ipAddrString[5] = "abcd";
+percentPtr = strchr(ipAddrString, '%');
+if (percentPtr != NILPTR)
+{
+*percentPtr = 0;
+} else {
+printf("Null Pointer");
+}
+return 0;
+}
